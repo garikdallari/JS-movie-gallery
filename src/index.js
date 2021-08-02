5444dac35951a6f1f8a129cd10ea8da1ff93e216
garikdallari <garikdallari@gmail.com>
2021-08-02 18:58:54 +0300

    test api request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+// API 93e18502a4f670f89316c5fc1b091bd6
+
+const axios = require('axios');
+const url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=93e18502a4f670f89316c5fc1b091bd6&language=en-US&page=1'
+
+async function getMovies() {
+    const response = await axios.get(url)
+    console.log(response)
+}
+getMovies()

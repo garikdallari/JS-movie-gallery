86cdab5bf1a8d0ad34c323a94ff48e827850c299
garikdallari <garikdallari@gmail.com>
2021-08-02 19:04:52 +0300

    test api request

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const axios = require('axios');
 const url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=93e18502a4f670f89316c5fc1b091bd6&language=en-US&page=1'
 
 async function getMovies() {
-    const response = await axios.get(url)
-    console.log(response)
+    const {data} = await axios.get(url)
+    
+    console.log(data.results)
 }
 getMovies()

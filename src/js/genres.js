e703123913dc851c8964a5817152c01a0277ae41
Olek Myron <myrondell@gmail.com>
2021-08-08 00:02:41 +0300

    added editGenres() & editDate()

--- src/js/genres.js
+++ src/js/genres.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL, API_KEY } from './searchProps';
+// import { BASE_URL, API_KEY } from './searchProps';
 import MovieApiService from './movieService';
 
 const movieApiService = new MovieApiService();
@@ -9,37 +9,7 @@ movieApiService.fetchGenres().then(res => {
 });
 
 // ===== parse genres from localStorage
-const genresList = localStorage.getItem('genresList');
-const genresObj = JSON.parse(genresList);
-
-// function fetchGenres() {
-//   return axios
-//     .get(`genre/movie/list?api_key=${API_KEY}&language=en-US`)
-//     .then(res => res.data.genres);
-// }
-
-// ===============================================
-function editDate(obj) {
-  const yearRef = document.querySelector(`[data-year-id="${obj.id}"]`);
-  const yearVal = yearRef.textContent;
-  yearRef.textContent = `| ${yearVal.slice(0, 4)}`;
-}
-
-function editGenres(obj) {
-  const genresRef = document.querySelector(`[data-id="${obj.id}"]`);
-  const parsedGenres = [];
-
-  for (let i = 0; i < obj.genre_ids.length; i += 1) {
-    obj.genre_ids.map(genre => {
-      genresObj.forEach(genreSet => {
-        if (genre === genreSet.id) {
-          parsedGenres.push(genreSet.name);
-        }
-      });
-    });
-  }
-
-  if (parsedGenres.length > 2)
-    return (genresRef.innerHTML = parsedGenres.splice(0, 3).join(', ') + ' ...');
-  genresRef.innerHTML = parsedGenres.join(', ');
+export function parseGenres() {
+  const genresList = localStorage.getItem('genresList');
+  return JSON.parse(genresList);
 }
--- src/js/movieService.js
+++ src/js/movieService.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 import { BASE_URL, API_KEY, TRENDING, SEARCH_MOVIE } from './searchProps';
+import { parseGenres } from './genres';
+
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'; // ---------------- удалить перед пулом
 
 export default class MovieApiService {
@@ -18,5 +20,31 @@ export default class MovieApiService {
     const res = await axios.get(`genre/movie/list?api_key=${API_KEY}&language=en-US`);
     const genres = await res.data.genres;
     return genres;
+  }
+
+  editGenres(obj) {
+    const genresRef = document.querySelector(`[data-id="${obj.id}"]`);
+    const parsedGenres = [];
+
+    for (let i = 0; i < obj.genre_ids.length; i += 1) {
+      obj.genre_ids.map(genre => {
+        const genresObj = parseGenres();
+        genresObj.forEach(genreSet => {
+          if (genre === genreSet.id) {
+            parsedGenres.push(genreSet.name);
+          }
+        });
+      });
+    }
+
+    if (parsedGenres.length > 2)
+      return (genresRef.innerHTML = parsedGenres.splice(0, 3).join(', ') + ' ...');
+    genresRef.innerHTML = parsedGenres.join(', ');
+  }
+
+  editDate(obj) {
+    const yearRef = document.querySelector(`[data-year-id="${obj.id}"]`);
+    const yearVal = yearRef.textContent;
+    yearRef.textContent = `| ${yearVal.slice(0, 4)}`;
   }
 }

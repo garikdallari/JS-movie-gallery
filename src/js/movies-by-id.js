ba7c41ccb436f1df84d3c1bf0f86a0bfbd3f4eed
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-08-13 18:24:45 +0300

    change code

--- src/js/movies-by-id.js
+++ src/js/movies-by-id.js
@@ -1,3 +1,4 @@
+import { Notify} from "notiflix";
 import MovieApiService from './movieService';
 import refs from './refs';
 import movieCard from '../templates/movie-popup.hbs';
@@ -20,7 +21,7 @@ const overley = document.querySelector('.lightbox__overlay');
 galleryRef.addEventListener('click', openModalOnClick);
 
 function openModalOnClick(e) {
-  e.preventDefault();
+  // e.preventDefault();
 
   if (!e.target.classList.contains('cards-list__img')) {
     return;
@@ -34,22 +35,8 @@ function openModalOnClick(e) {
     fetchMovieById();
     content.addEventListener('click',openTrailer)
     
-    content.addEventListener('click', closeTrailer)
 };
 
-
-function openTrailer(e) {
-        if (e.target.classList.contains("button_open")) {
-            document.querySelector(".plyr__video-embed").style.display = 'block';
-        
-        
-        }
-}
-function closeTrailer(e) {
-    if(e.target.classList.contains("trailer_button")) {
-   document.querySelector(".plyr__video-embed").style.display = 'none';
-    }
-}
 function closeModalOnClick() {
 
     modal.classList.remove('is-open');
@@ -72,6 +59,52 @@ function closeModalOnEsc(e) {
   }
 }
 
+
+function openTrailer(e) {
+  if (!e.target.classList.contains("button_open")) {
+    return;
+  }
+   document.querySelector(".plyr__video-embed").style.display = 'block';
+   const backdrop = document.querySelector(".backdrop");
+   backdrop.classList.add("backdrop-is-open");
+   const btnCloseTrailer = document.querySelector(".trailer_button")
+   btnCloseTrailer.addEventListener("click", closeTrailer);
+   const overlayForTrailer = document.querySelector(".backdrop_overlay");
+   overlayForTrailer.addEventListener("click", closeTrailer);
+           
+        
+        
+}
+
+
+
+function closeTrailer(e) {
+  console.log(e.target)
+  
+  document.querySelector(".plyr__video-embed").style.display = 'none';
+  const backdrop = document.querySelector(".backdrop");
+  backdrop.classList.remove("backdrop-is-open");
+  const overlayForTrailer = document.querySelector(".backdrop_overlay");
+  overlayForTrailer.removeEventListener("click", closeTrailer);
+    
+   const btnCloseTrailer = document.querySelector(".trailer_button")
+   btnCloseTrailer.removeEventListener("click", closeTrailer);
+    stopPlayer();
+  
+ 
+};
+function stopPlayer() {
+      const  iframe = document.querySelector('.ytplayer');
+        let src = iframe.getAttribute('src');
+
+        iframe.setAttribute('src', '');
+      
+        iframe.setAttribute('src', src);
+        }
+
+
+
+
 async function fetchMovieById() {
 
 const { data} = await movieApiService.getMovieInfo();
@@ -81,8 +114,10 @@ const genres=data.genres.slice(0,3).map(genre=>genre.name).join(" ");
 const  {results} = await movieApiService.fetchTrailer()
   let key;
     if(results.length === 0){
-        key='W9nZ6u15yis';
-       movieApiService.markupTempl(({data,genres,key}), content, movieCard); 
+      key='W9nZ6u15yis';
+      movieApiService.markupTempl(({ data, genres, key }), content, movieCard);
+      Notify.init({ distance:"150px",fontSize:"15px", warning: {background:"#ff6f09",}, }); 
+      Notify.warning("Sorry!We  don't have a trailer for this movie.");
     }
     else{
        key = results[0].key; 
